ccdd484d86e2b4a32291c8c6ab9a767a66cd59a0
vpolets <46418908+vpolets@users.noreply.github.com>
2019-05-14 09:47:03 +0300

    Update setOfPeople.js

--- src/setOfPeople.js
+++ src/setOfPeople.js
@@ -39,9 +39,10 @@
  */
 function setOfPeople(people) {
   const set = new Set();
-  people.forEach(person => set.has(person) ?
-                 set.delete(person) :
-                 set.add(person));
+  people.forEach(person => set.has(person) 
+    ? set.delete(person) 
+    : set.add(person)
+  );
   return set;
 }
 
