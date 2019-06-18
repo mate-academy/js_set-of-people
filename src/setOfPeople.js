7a49698dc993eb47b509035dee4e8fde04d918a9
neitiris <veliantos@gmail.com>
2019-06-18 16:07:01 +0300

    'solution'

--- src/setOfPeople.js
+++ src/setOfPeople.js
@@ -39,6 +39,15 @@
  */
 function setOfPeople(people) {
   // write code here
+  const room = new Set();
+  for (let i = 0; i < people.length; i++) {
+    if (room.has(people[i]) === false) {
+      room.add(people[i]);
+    } else if (room.has(people[i])) {
+      room.delete(people[i]);
+    }
+  }
+  return room;
 }
 
 module.exports = setOfPeople;
