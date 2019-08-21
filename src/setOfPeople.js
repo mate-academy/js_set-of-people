9f7691d6fb076cd17973107efa52db9bfdee3e9f
Vlad Grytsenko <vladik.grytsenko2017@gmail.com>
2019-08-21 13:03:15 +0300

    Solution

--- src/setOfPeople.js
+++ src/setOfPeople.js
@@ -38,7 +38,17 @@
  * @return {Set}
  */
 function setOfPeople(people) {
-  // write code here
+  const personsInRoom = new Set();
+
+  for (const person of people) {
+    if (personsInRoom.has(person)) {
+      personsInRoom.delete(person);
+    } else {
+      personsInRoom.add(person);
+    }
+  }
+
+  return personsInRoom;
 }
 
 module.exports = setOfPeople;
