1a9b879f10fda31842ff47cded77080fa780113e
Miroslava <777shadowpriest777@gmail.com>
2019-08-22 22:38:30 +0300

    Solution

--- src/setOfPeople.js
+++ src/setOfPeople.js
@@ -38,7 +38,15 @@
  * @return {Set}
  */
 function setOfPeople(people) {
-  // write code here
+  const peopleInRoom = new Set();
+
+  people.forEach((guest) => {
+    peopleInRoom.has(guest)
+      ? peopleInRoom.delete(guest)
+      : peopleInRoom.add(guest);
+  });
+
+  return peopleInRoom;
 }
 
 module.exports = setOfPeople;
