435c18ebacdd7c59125d6d973b40c34d0463a876
Dmytro <voronok16@gmail.com>
2019-08-23 15:21:56 +0300

    rewrite Solution

--- src/setOfPeople.js
+++ src/setOfPeople.js
@@ -38,33 +38,12 @@
  * @return {Set}
  */
 function setOfPeople(people) {
-  const set = new Set(people);
-  let key = 0;
-  const map = new Map();
-  const peopleInRoom = [];
-  let inOut = 0;
-
-  for (const value of set) {
-    for (let i = 0; i < people.length; i++) {
-      if (value === people[i]) {
-        inOut++;
-        people[i] = '';
-        key = i;
-      }
-    }
-    if (inOut % 2 !== 0) {
-      map.set(key, value);
-    }
-    inOut = 0;
-  }
-
-  const mapSort = new Map([...map.entries()].sort((a, b) => a[0] - b[0]));
-
-  for (const men of mapSort.values()) {
-    peopleInRoom.push(men);
-  }
-
-  return peopleInRoom;
+  const peopleInRoom = new Set();
+  people.forEach((inOut) =>
+    peopleInRoom.has(inOut)
+      ? peopleInRoom.delete(inOut)
+      : peopleInRoom.add(inOut));
+  return [...peopleInRoom];
 }
 
 module.exports = setOfPeople;
