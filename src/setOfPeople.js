1132c243f4a3709b383ce3932056fa9d95494f19
WincherZ <wincher2016@gmail.com>
2020-04-07 21:05:29 +0300

    some changes

--- src/setOfPeople.js
+++ src/setOfPeople.js
@@ -38,7 +38,13 @@
  * @return {Set}
  */
 function setOfPeople(people) {
-  // write code here
+  const set = new Set();
+
+  for (const item of people) {
+    set.has(item) ? set.delete(item) : set.add(item);
+  }
+
+  return set;
 }
 
 module.exports = setOfPeople;
