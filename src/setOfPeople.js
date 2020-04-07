575421b6eb5a2498bdbd0d0b6a34acfa5dde3430
Nick <nikolyaoooyartchuk@gmail.com>
2020-04-07 03:12:04 +0300

    solution

--- src/setOfPeople.js
+++ src/setOfPeople.js
@@ -38,7 +38,15 @@
  * @return {Set}
  */
 function setOfPeople(people) {
-  // write code here
+  const room = new Set();
+
+  people.map(a => {
+    return room.has(a)
+      ? room.delete(a)
+      : room.add(a);
+  });
+
+  return room;
 }
 
 module.exports = setOfPeople;
