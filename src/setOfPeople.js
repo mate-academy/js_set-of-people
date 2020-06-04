085802dc9e4e1e8d5c61a70ee7d871613f37c49c
tyooma <flying.alien.godzilla@gmail.com>
2020-06-04 14:40:46 +0300

    set of people solution

--- src/setOfPeople.js
+++ src/setOfPeople.js
@@ -38,7 +38,17 @@
  * @return {Set}
  */
 function setOfPeople(people) {
-  // write code here
+  const peopleInRoom = new Set();
+
+  for (const person of people) {
+    if (peopleInRoom.has(person)) {
+      peopleInRoom.delete(person);
+    } else {
+      peopleInRoom.add(person);
+    }
+  }
+
+  return peopleInRoom;
 }
 
 module.exports = setOfPeople;
