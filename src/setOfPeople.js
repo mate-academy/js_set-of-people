c53baf52c8a1c9d965ac5ba1659765c1efd8dca1
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-08-20 16:04:13 +0300

    solution

--- src/setOfPeople.js
+++ src/setOfPeople.js
@@ -38,7 +38,13 @@
  * @return {Set}
  */
 function setOfPeople(people) {
-  // write code here
+  const peopleInRoom = new Set();
+
+  people.forEach(person => peopleInRoom.has(person)
+    ? peopleInRoom.delete(person)
+    : peopleInRoom.add(person));
+
+  return peopleInRoom;
 }
 
 module.exports = setOfPeople;
