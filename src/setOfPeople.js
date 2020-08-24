563f7010aea54626ec99d715feb99256210f6a3a
Anton <tsymbal044@gmail.com>
2020-08-24 18:37:41 +0300

    solved

--- src/setOfPeople.js
+++ src/setOfPeople.js
@@ -38,7 +38,15 @@
  * @return {Set}
  */
 function setOfPeople(people) {
-  // write code here
+  const queue = new Set();
+
+  for (const person of people) {
+    queue.has(person)
+      ? queue.delete(person)
+      : queue.add(person);
+  }
+
+  return queue;
 }
 
 module.exports = setOfPeople;
